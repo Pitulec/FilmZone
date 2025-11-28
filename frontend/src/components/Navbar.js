5c06b4f35967ecda6960ae7a69527d36b5c8eda3
Pitulec <96929600+Pitulec@users.noreply.github.com>
2025-11-28 14:57:13 +0100

    fix: Fixed navbar links

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -2,11 +2,6 @@ import React from "react";
 import { Search, User } from "lucide-react";
 
 export default function Navbar() {
-	const menu = [
-		{ label: "Search", icon: <Search className="w-5" />, href: "/search" },
-		{ label: "Sign In", icon: <User className="w-5" />, href: "/signin" },
-	];
-
 	return (
         <div className="flex justify-center">
             <nav className="z-50 fixed flex items-center justify-between outline outline-[#8D99AE] rounded-full px-7.5 py-2 top-5 bg-[#8d99ae21] gap-6 sm:min-w-xl">
@@ -17,13 +12,13 @@ export default function Navbar() {
 
                 <ul className="flex items-center gap-6 font-medium">
                     <li>
-                        <a href="/search" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
+                        <a href="/auth/search" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
                         <Search className="w-5" />
                         Search
                         </a>
                     </li>
                     <li>
-                        <a href="/signin" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
+                        <a href="/auth/signin" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
                         <User className="w-5" />
                         Sign In
                         </a>
