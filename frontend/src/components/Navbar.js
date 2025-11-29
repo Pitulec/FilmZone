6584dcec67bcb75b8df41b95f6dc3e59c83fa6c4
MarcelBernas <marcelbernas59@gmail.com>
2025-11-29 12:36:49 +0100

    Fixed film details page, changed navbar background color

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,29 +3,28 @@ import { Search, User } from "lucide-react";
 
 export default function Navbar() {
 	return (
-        <div className="flex justify-center">
-            <nav className="z-50 fixed flex items-center justify-between outline outline-[#8D99AE] rounded-full px-7.5 py-2 top-5 bg-[#8d99ae21] gap-6 sm:min-w-xl">
-                <a href="/" className="text-2xl font-bold">
-                    <span>Film</span>
-                    <span className="text-[#DD4242]">Zone</span>
-                </a>
+		<div className="flex justify-center">
+			<nav className="z-50 fixed flex items-center justify-between outline outline-[#8D99AE] rounded-full px-7.5 py-2 top-5 bg-[#2B2D42] gap-6 sm:min-w-xl">
+				<a href="/" className="text-2xl font-bold">
+					<span>Film</span>
+					<span className="text-[#DD4242]">Zone</span>
+				</a>
 
-                <ul className="flex items-center gap-6 font-medium">
-                    <li>
-                        <a href="/auth/search" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
-                        <Search className="w-5" />
-                        Search
-                        </a>
-                    </li>
-                    <li>
-                        <a href="/auth/signin" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
-                        <User className="w-5" />
-                        Sign In
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
-
+				<ul className="flex items-center gap-6 font-medium">
+					<li>
+						<a href="/auth/search" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
+							<Search className="w-5" />
+							Search
+						</a>
+					</li>
+					<li>
+						<a href="/auth/signin" className="flex items-center gap-2 hover:text-[#DD4242] transition-all">
+							<User className="w-5" />
+							Sign In
+						</a>
+					</li>
+				</ul>
+			</nav>
+		</div>
 	);
 }
